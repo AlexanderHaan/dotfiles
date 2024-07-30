d7b6d32e04fee55a38335e2ff8cd7ca3fbdba451
AlexanderHaan <haan.alex@outlook.de>
2024-07-30 07:30:23 -0400

    better lsp

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ local plugin_treesitter = {
 
 		configs.setup({
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp",
-				"heex", "javascript", "html", "ruby", "php" },
+				"heex", "javascript", "html", "ruby", "php", "tsx" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },  
@@ -44,22 +44,56 @@ local plugin_telescope = {
 	end
 }
 
+local plugin_lspconfig = {
+	"neovim/nvim-lspconfig"
+}
 
 require("lazy").setup({
 	plugin_treesitter,
 	plugin_telescope,
+	plugin_lspconfig
 })
 
--- Lua settings
+local lspconfig = require('lspconfig')
+local on_attach = function(_, bufnr)
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'lua',
-  callback = function(ev)
-    vim.lsp.start({
-      cmd = {'lua-language-server'},
-    })
-  end,
-})
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+end
+
+lspconfig.lua_ls.setup {
+	on_attach = on_attach,
+}
+
+lspconfig.jedi_language_server.setup {
+	on_attach = on_attach,
+}
+
+lspconfig.clangd.setup {
+	on_attach = on_attach,
+}
 
 vim.opt.termguicolors = true
 vim.opt.list = true
